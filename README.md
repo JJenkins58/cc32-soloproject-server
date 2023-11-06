e2cafb7b50aeb08c22eaf00daf21a4e72069aeb3
John Jenkins <knights70@gmail.com>
2023-11-06 19:05:36 +0900

    changed from ts to js

--- README.md
+++
@@ -1,1 +0,0 @@
-# cc32-soloproject-server
---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const cors = require("cors");
+const knex = require('./knex');
+
+const PORT = process.env.PORT || 8080;
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(PORT, () => console.log(`server is listening on port ${PORT}`));
+
+app.get("/hello", (req, res) => {
+    const helloWorld = "Hello World from server";
+    res.status(200).send(JSON.stringify(helloWorld));
+});

bee900740f98002e648c20f3e4cae51f5639b391
John Jenkins <knights70@gmail.com>
2023-11-07 17:20:14 +0900

    feat: Added accountController to server

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const cors = require("cors");
 
+const accountController = require("./src/account/account-controller");
+
 const PORT = process.env.PORT || 8080;
 
 app.use(express.json());

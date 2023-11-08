29b5c20388704667eb87f431f214f1fd5736297b
John Jenkins <knights70@gmail.com>
2023-11-08 11:00:15 +0900

    feat: updated knexfile connection to render

--- knexfile.js
+++ knexfile.js
@@ -1,14 +1,17 @@
 require("dotenv").config({path: "./.env.local"});
 
-const connection = process.env.DB_NAME ?
+const connection = process.env.DB_URL || 
 {
   host: process.env.DB_HOST || "127.0.0.1",
   port: process.env.DB_PORT || 5432,
   database: process.env.DB_NAME,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD
-}
-: process.env.DB_URL;
+};
+
+// ?
+
+// : process.env.DB_URL;
 
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
